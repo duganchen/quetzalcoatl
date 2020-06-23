04be6fcd167db9af06cadd4de282c1737e9c85ea
Dugan Chen <thedoogster@gmail.com>
2020-06-23 12:08:39 -0700

    Unit tests and the application both run

--- app/main.cpp
+++ app/main.cpp
@@ -8,16 +8,6 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    Controller controller("localhost", 6600, 1000);
-    QObject::connect(&w,
-                     &MainWindow::listAlbumsClicked,
-                     &controller,
-                     &Controller::handleListAlbumsClick);
-    QObject::connect(&w, &MainWindow::connectClicked, &controller, &Controller::handleConnectClick);
-    QObject::connect(&controller, &Controller::connectionState, &w, &MainWindow::setConnectionState);
-
-    QObject::connect(&controller, &Controller::unrecoverableError, &a, &QApplication::quit);
-
     w.show();
     return a.exec();
 }
