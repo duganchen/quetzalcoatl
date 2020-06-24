d30f2cdb1cb4aac2cb452e4642428480343fcef7
Dugan Chen <thedoogster@gmail.com>
2020-06-24 08:49:39 -0700

    Put widgets in the Save Playlist dialog

--- app/saveplaylistdialog.cpp
+++ app/saveplaylistdialog.cpp
@@ -1,11 +1,24 @@
-#include <QLineEdit>
-#include <QDialogButtonBox>
-#include <QLineEdit>
-#include <QRegularExpressionValidator>
-#include <QVBoxLayout>
 #include "saveplaylistdialog.h"
+#include <QDialogButtonBox>
+#include <QFormLayout>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QVBoxLayout>
 
-SavePlaylistDialog::SavePlaylistDialog(QWidget *parent, Qt::WindowFlags f) : QDialog(parent, f)
+SavePlaylistDialog::SavePlaylistDialog(QWidget *parent, Qt::WindowFlags f)
+    : QDialog(parent, f)
 {
-
+    setWindowTitle("Save Playlist");
+    auto layout = new QVBoxLayout();
+    auto formLayout = new QFormLayout();
+    formLayout->addRow("&Name", new QLineEdit());
+    layout->addLayout(formLayout);
+    auto buttonBox = new QDialogButtonBox();
+    auto okButton = buttonBox->addButton(QDialogButtonBox::Ok);
+    connect(okButton, &QPushButton::clicked, this, &QDialog::accept);
+    layout->addWidget(buttonBox);
+    auto cancelButton = buttonBox->addButton(QDialogButtonBox::Cancel);
+    connect(cancelButton, &QPushButton::clicked, this, &QDialog::reject);
+    layout->addWidget(buttonBox);
+    setLayout(layout);
 }
