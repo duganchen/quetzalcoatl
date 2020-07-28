0bbf0280160a29b8a1e092f1066261dd904e2b3b
Dugan Chen <thedoogster@gmail.com>
2020-07-28 14:49:51 -0700

    Did I forget to commit a file?

---
+++ controller/unorderedmpdsongitem.cpp
@@ -0,0 +1,22 @@
+#include "unorderedmpdsongitem.h"
+
+UnorderedMPDSongItem::UnorderedMPDSongItem(mpd_song *song, Item *parent)
+    : UnorderedSongItem(QIcon(":/icons/audio-x-generic.svg"),
+                        Qt::ItemIsEnabled | Qt::ItemIsSelectable | Qt::ItemIsDragEnabled,
+                        false,
+                        false,
+                        parent)
+    , m_song(song)
+{}
+
+UnorderedMPDSongItem::~UnorderedMPDSongItem()
+{
+    if (m_song) {
+        mpd_song_free(m_song);
+    }
+}
+
+const mpd_song *UnorderedMPDSongItem::song() const
+{
+    return m_song;
+}
