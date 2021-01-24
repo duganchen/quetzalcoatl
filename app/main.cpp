be6b44650f0e57e72f9bd56bc5ec856b98367288
Dugan Chen <thedoogster@gmail.com>
2021-01-23 20:48:22 -0800

    Fix a comment typo

--- app/main.cpp
+++ app/main.cpp
@@ -15,10 +15,10 @@ int main(int argc, char *argv[])
     QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
-    QApplication a(argc, argv);
+    QApplication app(argc, argv);
 
     // I'm just going to make this a single-instance application. This avoids so many bugs...
-    // Implemetation is from here:
+    // Implementation is from here:
     // http://blog.aeguana.com/2015/10/15/how-to-run-a-single-app-instance-in-qt/
 
     auto tmpDir = QDir::tempPath();
@@ -26,14 +26,14 @@ int main(int argc, char *argv[])
 
     if (!lockFile.tryLock(100)) {
         QMessageBox msgBox;
-        msgBox.setIcon(QMessageBox::Warning);
+        msgBox.msgBox.setIcon(QMessageBox::Warning);
         msgBox.setText("You already have Quetzalcoatl running."
                        "\r\nOnly one instance is allowed.");
         msgBox.exec();
         return 1;
     }
 
-    MainWindow w;
-    w.show();
-    return a.exec();
+    MainWindow window;
+    window.show();
+    return app.exec();
 }
